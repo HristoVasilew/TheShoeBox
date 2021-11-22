5d1a32d5fc21afdaa7c6bcd1493dcde8ee0d95aa
HristoVasilew <hristo.vasilev772@gmail.com>
2021-11-22 15:35:42 +0200

    Initial commit

---
+++ src/main/java/TheShoeBox/TheShoeBox/TheShoeBoxApplication.java
@@ -0,0 +1,13 @@
+package TheShoeBox.TheShoeBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheShoeBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheShoeBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TheShoeBox/TheShoeBox/TheShoeBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package TheShoeBox.TheShoeBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheShoeBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
