076d07bdca8b5dbbabbf3b4b5478634276d40fb7
HristoVasilew <hristo.vasilev772@gmail.com>
2021-12-04 11:43:20 +0200

    cloudinary tests

--- src/main/java/TheShoeBox/TheShoeBox/config/ApplicationBeanConfiguration.java
+++ src/main/java/TheShoeBox/TheShoeBox/config/ApplicationBeanConfiguration.java
@@ -1,13 +1,22 @@
 package TheShoeBox.TheShoeBox.config;
 
+import com.cloudinary.Cloudinary;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
+import java.util.Map;
+
 @Configuration
 public class ApplicationBeanConfiguration {
+
+    private final CloudinaryConfiguration config;
+
+    public ApplicationBeanConfiguration(CloudinaryConfiguration config) {
+        this.config = config;
+    }
 
     @Bean
     public ModelMapper modelMapper() {
@@ -17,6 +26,17 @@ public class ApplicationBeanConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Pbkdf2PasswordEncoder();
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(
+                Map.of(
+                        "cloud_name", config.getCloudName(),
+                        "api_key", config.getApiKey(),
+                        "api_secret", config.getApiSecret()
+                )
+        );
     }
 
 
---
+++ src/main/java/TheShoeBox/TheShoeBox/service/CloudinaryService.java
@@ -0,0 +1,16 @@
+package TheShoeBox.TheShoeBox.service;
+
+import TheShoeBox.TheShoeBox.model.service.CloudinaryImage;
+import TheShoeBox.TheShoeBox.service.impl.CloudinaryServiceImpl;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface CloudinaryService {
+
+    CloudinaryImage upload(MultipartFile file) throws IOException;
+
+    Boolean delete(String publicId);
+
+    CloudinaryServiceImpl setFolderName(String folderName);
+}
